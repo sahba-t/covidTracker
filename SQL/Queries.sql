5f3a3d7363648c8a1f2ced727ea7d730a269bebb
sahba t <stashakkori@unm.edu>
2020-05-11 17:55:44 -0600

    added icu rate vs death per capita

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -18,3 +18,8 @@ GROUP BY FAC.State, FAC.DateRecorded, US.Positive, US.Negative, US.Total, US.Dea
 ORDER BY FAC.State, FAC.DateRecorded
 ) where daterecorded IN(select max(daterecorded) from USCASebystate)
 ORDER BY state;
+
+/* getting ICU occupancy rate, population and death rates*/
+select f.state, f.daterecorded, total_pop, icu_avg, j.death_tot from(select f.state, f.daterecorded, sum(population) as total_pop from USFACILITIES f where  daterecorded=to_date('05/09/2020','MM/DD/YYYY')  group by state, daterecorded) f , (select o.state, o.daterecorded, round(avg(icubedoccupancyrate),3) as icu_avg from usicubedoccupancy o where o.daterecorded=to_date('05/09/2020','MM/DD/YYYY')  group by o.state, daterecorded) o, (select sum(deaths) as death_tot, j.state, j.daterecorded from jhudata j where daterecorded=to_date('05/09/2020','MM/DD/YYYY')   group by state, daterecorded) j where f.state=o.state and j.state=o.state;
+
+
